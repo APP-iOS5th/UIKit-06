e57a81b125360eabe85cf730f58f8f3b28c4511a
jmbae <jmbae@codegrove.co.kr>
2024-05-13 09:32:22 +0900

    뷰컨트롤러 이름 변경, MapViewController 추가

--- JRNL-Codeonly/JRNL-Codeonly/SceneDelegate.swift
+++ JRNL-Codeonly/JRNL-Codeonly/SceneDelegate.swift
@@ -12,13 +12,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        // self.window = UIWindow(frame: UIWindow.main.bounds)
+        // self.window?.windowScene = windowScene
+        // 아래 코드와 동일
         self.window = UIWindow(windowScene: windowScene)
-        self.window?.rootViewController = ViewController()
+        
+        
+        self.window?.rootViewController = JournalListViewController()
         self.window?.makeKeyAndVisible()
     }
 
