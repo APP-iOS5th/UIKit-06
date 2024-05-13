544a14278107176e148450e12bd75f7569220dcd
jmbae <jmbae@codegrove.co.kr>
2024-05-13 16:43:58 +0900

    New Entry 입력 컴포넌트 추가

--- JRNL-Codeonly/JRNL-Codeonly/AddJournalViewController.swift
+++ JRNL-Codeonly/JRNL-Codeonly/AddJournalViewController.swift
@@ -30,7 +30,7 @@ class AddJournalViewController: UIViewController {
         stackView.axis = .horizontal
         stackView.alignment = .fill
         stackView.distribution = .fill
-        
+        stackView.spacing = 8
         let switchComponent = UISwitch()
         let labelComponent = UILabel()
         labelComponent.text = "Switch Label"
@@ -40,28 +40,54 @@ class AddJournalViewController: UIViewController {
         return stackView
     }()
     
+    private lazy var titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Journal Title"
+        return textField
+    }()
+    
+    private lazy var bodyTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Journal Body"
+        return textView
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "face.smiling")
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         navigationItem.title = "New Entry"
         view.backgroundColor = .white
-
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save,
                                                             target: self,
                                                             action: #selector(save))
-
+        
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel,
                                                            target: self,
                                                            action: #selector(cancel))
         
         mainContainer.addArrangedSubview(ratingView)
         mainContainer.addArrangedSubview(toggleView)
+        mainContainer.addArrangedSubview(titleTextField)
+        mainContainer.addArrangedSubview(bodyTextView)
+        mainContainer.addArrangedSubview(imageView)
         
         view.addSubview(mainContainer)
         
         let safeArea = view.safeAreaLayoutGuide
         
         mainContainer.translatesAutoresizingMaskIntoConstraints = false
+        ratingView.translatesAutoresizingMaskIntoConstraints = false
+        toggleView.translatesAutoresizingMaskIntoConstraints = false
+        titleTextField.translatesAutoresizingMaskIntoConstraints = false
+        bodyTextView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             mainContainer.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20),
@@ -69,28 +95,39 @@ class AddJournalViewController: UIViewController {
             mainContainer.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20),
             
             ratingView.widthAnchor.constraint(equalToConstant: 252),
-            ratingView.heightAnchor.constraint(equalToConstant: 44)
+            ratingView.heightAnchor.constraint(equalToConstant: 44),
+            
+            titleTextField.leadingAnchor.constraint(equalTo: mainContainer.leadingAnchor, constant: 8),
+            titleTextField.trailingAnchor.constraint(equalTo: mainContainer.trailingAnchor, constant: -8),
+            
+            bodyTextView.leadingAnchor.constraint(equalTo: mainContainer.leadingAnchor, constant: 8),
+            bodyTextView.trailingAnchor.constraint(equalTo: mainContainer.trailingAnchor, constant: -8),
+            bodyTextView.heightAnchor.constraint(equalToConstant: 128),
+            
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: 200)
+
         ])
         
     }
     
     @objc func save() {
-
+        
     }
     
     @objc func cancel() {
         dismiss(animated: true)
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
