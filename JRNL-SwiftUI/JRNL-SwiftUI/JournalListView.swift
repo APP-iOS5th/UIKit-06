8107ae9441c550924da7fa6de4d529fc04a644a5
jmbae <jmbae@codegrove.co.kr>
2024-05-28 17:07:48 +0900

    Tab 뷰 추가

---
+++ JRNL-SwiftUI/JRNL-SwiftUI/JournalListView.swift
@@ -0,0 +1,18 @@
+//
+//  JournalListView.swift
+//  JRNL-SwiftUI
+//
+//  Created by Jungman Bae on 5/28/24.
+//
+
+import SwiftUI
+
+struct JournalListView: View {
+    var body: some View {
+        Text("Journal List")
+    }
+}
+
+#Preview {
+    JournalListView()
+}
