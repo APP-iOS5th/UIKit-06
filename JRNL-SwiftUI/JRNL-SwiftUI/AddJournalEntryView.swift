36ceffd04b0613b6db114931bad35cfde24b99e8
jmbae <jmbae@codegrove.co.kr>
2024-05-28 17:29:32 +0900

    Add Entry 기본 입력 폼 추가

--- JRNL-SwiftUI/JRNL-SwiftUI/AddJournalEntryView.swift
+++ JRNL-SwiftUI/JRNL-SwiftUI/AddJournalEntryView.swift
@@ -9,23 +9,45 @@ import SwiftUI
 
 struct AddJournalEntryView: View {
     @Environment(\.dismiss) var dismiss
+    
+    @State private var isGetLocationOn = false
+    @State private var entryTitle = ""
+    @State private var entryBody = ""
+    
     var body: some View {
         NavigationStack {
-            Text("Add JournalEntry")
-                .navigationTitle("Add Journal Entry")
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        Button("Cancel") {
-                            dismiss()
-                        }
-                    }
-                    ToolbarItem(placement: .topBarLeading) {
-                        Button("Save") {
-                            dismiss()
-                        }
+            Form {
+                Section(header: Text("Rating")) {
+                    Rectangle()
+                        .foregroundStyle(.cyan)
+                }
+                Section(header: Text("Location")) {
+                    Toggle("Get Location", isOn: $isGetLocationOn)
+                }
+                Section(header: Text("Title")) {
+                    TextField("Enter title", text: $entryTitle)
+                }
+                Section(header: Text("Body")) {
+                    TextEditor(text: $entryBody)
+                }
+                Section(header: Text("Photo")) {
+                    
+                }
+            }
+            .navigationTitle("Add Journal Entry")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
                     }
                 }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Save") {
+                        dismiss()
+                    }
+                }
+            }
         }
     }
 }
