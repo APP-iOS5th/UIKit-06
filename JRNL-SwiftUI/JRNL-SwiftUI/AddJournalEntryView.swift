9eacf820f14b4283bd20e68346bc269556638e55
jmbae <jmbae@codegrove.co.kr>
2024-05-28 17:20:21 +0900

    AddJournal View 생성

---
+++ JRNL-SwiftUI/JRNL-SwiftUI/AddJournalEntryView.swift
@@ -0,0 +1,35 @@
+//
+//  AddJournalEntryView.swift
+//  JRNL-SwiftUI
+//
+//  Created by Jungman Bae on 5/28/24.
+//
+
+import SwiftUI
+
+struct AddJournalEntryView: View {
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+        NavigationStack {
+            Text("Add JournalEntry")
+                .navigationTitle("Add Journal Entry")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button("Cancel") {
+                            dismiss()
+                        }
+                    }
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button("Save") {
+                            dismiss()
+                        }
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    AddJournalEntryView()
+}
